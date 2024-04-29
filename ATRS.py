19439c381580dae1305be3a5fdde2196b95a52e4
ec4016 <112779974+ec4016@users.noreply.github.com>
2024-04-29 13:02:10 -0400

    yuyu's work

---
+++ ATRS.py
@@ -0,0 +1,185 @@
+# Import Flask Library
+import hashlib
+
+from flask import Flask, render_template, request, session, url_for, redirect
+import pymysql.cursors
+
+# Initialize the app from Flask
+app = Flask(__name__)
+
+# Configure MySQL
+conn = pymysql.connect(host='localhost',
+                       user='root',
+                       password='',
+                       db='air ticket reservation system',
+                       charset='utf8mb4',
+                       cursorclass=pymysql.cursors.DictCursor)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login_customer')
+def login_customer():
+    return render_template('login_customer.html')
+
+
+@app.route('/cust_loginAuth', methods=['POST'])
+def cust_loginAuth():
+    username = request.form['username']
+    password = request.form['password']
+    hashed_password = hashlib.md5(password.encode()).hexdigest()
+    cursor = conn.cursor()
+    query = "SELECT * FROM customer WHERE email = %s AND password = %s"
+    cursor.execute(query, (username, hashed_password))
+    data = cursor.fetchone()
+    error = None
+    cursor.close()
+    if (data):
+        session['username'] = username
+        return redirect(url_for('home_customer'))
+    else:
+        error = 'Invalid login or username'
+        return render_template('login_customer.html', error=error)
+
+
+@app.route('/login_staff')
+def login_staff():
+    return render_template('login_staff.html')
+
+
+@app.route('/staff_loginAuth', methods=['POST'])
+def staff_loginAuth():
+    username = request.form['username']
+    password = request.form['password']
+    hashed_password = hashlib.md5(password.encode()).hexdigest()
+    cursor = conn.cursor()
+    query = "SELECT * FROM staff WHERE username = %s AND password = %s"
+    cursor.execute(query, (username, hashed_password))
+    data = cursor.fetchone()
+    cursor.close()
+    error = None
+    if (data):
+        session['username'] = username
+        return redirect(url_for('home_staff'))
+    else:
+        error = 'Invalid login or username'
+        return render_template('login_staff.html', error=error)
+
+
+@app.route('/register_customer')
+def register_customer():
+    return render_template('register_customer.html')
+
+
+@app.route('/cust_registerAuth', methods=['POST'])
+def cust_registerAuth():
+    username = request.form['username']
+    cursor = conn.cursor()
+    query = 'SELECT * FROM customer WHERE email = %s'
+    cursor.execute(query, (username))
+    data = cursor.fetchone()
+    error = None
+    if (data):
+        error = "This user already exists"
+        return render_template('register_customer.html', error=error)
+    else:
+        ins = """INSERT INTO customer (email, first_name, last_name, password, building_num, 
+                                 street, apartment_num, city, state, zip_code, primary_phone_number, passport_number, 
+                                 passport_expiration_date, passport_country, date_of_birth)
+                                 VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+        hashed_password = hashlib.md5(request.form['password'].encode()).hexdigest()
+        cursor.execute(ins, (request.form['username'], request.form['first_name'], request.form['last_name'],
+                             hashed_password, request.form['building_num'], request.form['street'],
+                             request.form['apartment_num'], request.form['city'], request.form['state'],
+                             request.form['zip_code'], request.form['primary_phone_number'],
+                             request.form['passport_number'], request.form['passport_expiration_date'],
+                             request.form['passport_country'], request.form['date_of_birth']))
+        conn.commit()
+        ins2='INSERT INTO customer_phone_numbers (email, phone_number) VALUES (%s,%s)'
+        cursor.execute(ins2,(request.form['username'],request.form['primary_phone_number']))
+        conn.commit()
+        cursor.close()
+        return render_template('index.html')
+
+
+@app.route('/register_staff')
+def register_staff():
+    return render_template('register_staff.html')
+
+
+@app.route('/staff_registerAuth', methods=['POST'])
+def staff_registerAuth():
+    username = request.form['username']
+    cursor = conn.cursor()
+    query = 'SELECT * FROM staff WHERE username = %s'
+    cursor.execute(query, (username))
+    data = cursor.fetchone()
+    error = None
+    if (data):
+        error = "This user already exists"
+        return render_template('register_staff.html', error=error)
+    else:
+        ins = """INSERT INTO staff (username, password, first_name, last_name, date_of_birth, 
+                         primary_email, airline_name)
+                         VALUES (%s, %s, %s, %s, %s, %s, %s)"""
+        hashed_password = hashlib.md5(request.form['password'].encode()).hexdigest()
+        cursor.execute(ins, (request.form['username'], hashed_password, request.form['first_name'],
+                             request.form['last_name'], request.form['date_of_birth'],
+                             request.form['primary_email'], request.form['airline_name']))
+        conn.commit()
+        cursor.close()
+        return render_template('index.html')
+
+@app.route('/home_customer')
+def home_customer():
+    username = session['username']
+    return render_template('home_customer.html', username=username)
+
+@app.route('/cust_add_num',methods=['POST'])
+def cust_add_num():
+    username = session['username']
+    number = request.form['phonenumber']
+    cursor = conn.cursor();
+    query = 'SELECT * FROM customer_phone_numbers WHERE email=%s AND phone_number=%s'
+    cursor.execute(query, (username,number))
+    data = cursor.fetchone()
+    error = None
+    if(data):
+        error = "This number already exists"
+        return render_template('home_customer.html', error=error)
+    else:
+        ins="INSERT INTO customer_phone_numbers (email, phone_number) VALUES (%s, %s)"
+        cursor.execute(ins, (username,number))
+        conn.commit()
+        cursor.close()
+        return render_template('home_customer.html')
+
+@app.route('/customer_flights')
+def customer_flights():
+    username=session['username']
+    cursor = conn.cursor();
+    query='SELECT airline_name, flight_num, departure_date, departure_time, arrival_date, arrival_time, status, ' \
+          'airplane_id, departure_airport, arrival_airport, sold_price, first_name, last_name'\
+          ' FROM flight NATURAL JOIN ticket NATURAL JOIN purchase' \
+          ' WHERE email=%s'
+    cursor.execute(query, username)
+    data = cursor.fetchall()
+    cursor.close()
+    return render_template('customer_flights.html',flights=data)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username')
+    return redirect('/')
+
+
+app.secret_key = 'some key that you will never guess'
+# Run the app on localhost port 5000
+# debug = True -> you don't have to restart flask
+# for changes to go through, TURN OFF FOR PRODUCTION
+if __name__ == "__main__":
+    app.run('127.0.0.1', 5000, debug=True)
---
+++ system_createtables.sql
@@ -0,0 +1,145 @@
+Create table airline ( 
+name varchar(30),
+primary key (name)
+);
+
+Create table airplane (
+airline_name varchar(30),
+airplane_id int,
+num_seats int not null,
+manufacturer varchar(30),
+model_number varchar(30),
+manufacturing_date DATE,
+age int default 0,
+primary key (airline_name, airplane_id),
+Foreign key (airline_name) references airline (name)
+);
+
+Create table staff (
+username varchar(30) not null,
+airline_name varchar(30),
+password varchar(32),
+first_name varchar(30) not null,
+last_name varchar(30) not null,
+date_of_birth date not null,
+primary_email varchar(30) not null,
+primary key (username),
+Foreign key (airline_name) references airline (name)
+);
+
+Create table staff_email (
+username 	varchar(30),
+email 		varchar(30),
+Primary key (username, email),
+Foreign key (username) references staff (username)
+);
+
+Create table staff_phone_numbers (
+username 		varchar(30),
+phone_number 	varchar(30),
+Primary key (username, phone_number),
+Foreign key (username) references staff (username)
+);
+
+Create table airport ( 
+code VARCHAR(30), 
+name varchar(30), 
+city varchar(30), 
+country varchar(30), 
+num_terminals int, 
+type varchar(30), 
+Primary key (code) 
+);
+
+CREATE TABLE Flight (
+airline_name VARCHAR(30),
+flight_num int,
+departure_date DATE,
+departure_time TIME,
+arrival_date DATE,
+arrival_time TIME,
+base_price numeric(9,2),
+status VARCHAR(30),
+airplane_id INT,
+departure_airport VARCHAR(30),
+arrival_airport VARCHAR(30),
+PRIMARY KEY(airline_name, flight_num, departure_date, departure_time),
+FOREIGN KEY (airline_name) REFERENCES Airline(name)
+);
+
+CREATE TABLE Customer (
+email VARCHAR(30),
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+password VARCHAR(32),
+building_num INT,
+street VARCHAR(30),
+apartment_num VARCHAR(30),
+city VARCHAR(30),
+state VARCHAR(30),
+zip_code INT,
+primary_phone_number varchar(30),
+passport_number INT,
+passport_expiration_date date,
+passport_country VARCHAR(30),
+date_of_birth DATE,
+PRIMARY KEY(email)
+);
+
+CREATE TABLE Flight_Taken (
+email VARCHAR(30),
+airline_name VARCHAR(30),
+flight_num int,
+departure_date DATE,
+departure_time TIME,
+rating numeric(2,1),
+comment VARCHAR(100),
+PRIMARY KEY(email, airline_name, flight_num, departure_date,  departure_time),
+FOREIGN KEY(email) REFERENCES Customer(email),
+FOREIGN KEY(airline_name, flight_num, departure_date,  departure_time) REFERENCES Flight(airline_name, flight_num, departure_date,  departure_time)
+);
+
+CREATE TABLE Ticket (
+ticket_id INT,
+flight_num int,
+sold_price INT,
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+date_of_birth DATE,
+airline_name varchar(30),
+departure_date date,
+departure_time time,
+PRIMARY KEY(ticket_id, flight_num),
+FOREIGN KEY(airline_name, flight_num, departure_date, departure_time) REFERENCES Flight(airline_name, flight_num, departure_date, departure_time)
+);
+
+CREATE TABLE Purchase(
+email VARCHAR(30),
+ticket_id INT,
+card_type VARCHAR(30),
+card_num bigint,
+expiration_date DATE,
+purchase_date DATE,
+purchase_time TIME,
+PRIMARY KEY(email, ticket_id),
+FOREIGN KEY(email) REFERENCES Customer(email),
+FOREIGN KEY(ticket_id) REFERENCES Ticket(ticket_id) 
+);
+
+CREATE TABLE Customer_Phone_Numbers (
+email VARCHAR(30),
+Phone_number VARCHAR(30),
+PRIMARY KEY(email, phone_number),
+FOREIGN KEY(email) REFERENCES Customer(email)
+);
+
+CREATE TABLE Maintenance (
+airline_name VARCHAR(30),
+airplane_id int,
+start_date DATE,
+start_time TIME,
+end_date DATE,
+end_time TIME,
+PRIMARY KEY(airline_name, airplane_id, start_date, start_time, end_date, end_time),
+FOREIGN KEY(airline_name, airplane_id) REFERENCES Airplane(airline_name, airplane_id)
+);
